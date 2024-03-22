58ce68f9858664f82c578c8fef032b43f08b2a36
Santiago Gomez Perdomo <santigomezp13@gmail.com>
2024-03-22 01:37:36 -0400

    firsttry

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,20 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send("im here")
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    return res.send(books[isbn])
  });
   
 // Get book details based on author
@@ -36,8 +34,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let book = books[isbn];
+  if (book){
+    let review = req.body.reviews;
+    res.send(book["reviews"])
+  }
+  
+
 });
 
 module.exports.general = public_users;
