3b49350fb2e39d10bc8d11ae228add25887097ca
Santiago Gomez <santigomezp13@gmail.com>
2024-03-23 00:39:02 -0400

    All Done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,35 +32,85 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books,null,4))
+//Task #1
+////return res.send(JSON.stringify(books,null,4))
+//Task #10
+let booksstored = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(JSON.stringify(books,null,4))
+    },1000)})
+    booksstored.then((successMessage) => {
+        return res.send(successMessage)
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-    return res.send(books[isbn])
+  //Task #2
+  ////const isbn = req.params.isbn;
+  ////return res.send(books[isbn])
+  //Task #11
+  let bookisbn = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      isbn = req.params.isbn;  
+      resolve(books[isbn])
+    },1000)})
+    bookisbn.then((successMessage) => {
+        return res.send(successMessage)
+    })
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const name= req.params.author.replaceAll("-"," ");
-    for (const key in books){
-    const author = books[key]["author"];
-    if (name == author){
-        res.send(books[key])
-    }
-    }
+    //Task #3
+    ////const name= req.params.author.replaceAll("-"," ");
+    ////for (const key in books){
+    ////const author = books[key]["author"];
+    ////if (name == author){
+    ////    res.send(books[key])
+    ////}
+    ////}
+    //Task#12
+    let bookauthor = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const name= req.params.author.replaceAll("-"," ");
+            for (const key in books){
+                const author = books[key]["author"];
+                if (name == author){
+                    resolve(books[key])
+                }
+            }
+        },1000)})
+        bookauthor.then((successMessage) => {
+            return res.send(successMessage)
+        })
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const name= req.params.title.replaceAll("-"," ");
-    for (const key in books){
-    const title = books[key]["title"];
-    if (name == title){
-        res.send(books[key])
-    }
-    }
+    //Task #4
+    ////const name= req.params.title.replaceAll("-"," ");
+    ////for (const key in books){
+    ////const title = books[key]["title"];
+    ////if (name == title){
+    ////    res.send(books[key])
+    ////}
+    ////}
+    //Task #13
+    let bookitem = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const name= req.params.title.replaceAll("-"," ");
+            for (const key in books){
+            const title = books[key]["title"];
+            if (name == title){
+                resolve(books[key])
+            }
+            }
+        },1000)})
+        bookitem.then((successMessage) => {
+            return res.send(successMessage)
+        })
 });
 
 //  Get book review
